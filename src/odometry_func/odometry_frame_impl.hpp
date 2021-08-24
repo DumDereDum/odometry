9a660c573b9287b5e3adf97872560446ae3d48ba
Saratovtsev <artem.saratovtsev@intel.com>
2021-08-24 07:40:44 +0300

    odometry frame update

--- src/odometry_func/odometry_frame_impl.hpp
+++ src/odometry_func/odometry_frame_impl.hpp
@@ -15,7 +15,10 @@ class OdometryFrameImpl
 public:
 	OdometryFrameImpl() {};
 	~OdometryFrameImpl() {};
-	virtual void setImage(InputArray image) = 0;
+	virtual void setImage(InputArray  image) = 0;
+	virtual void getImage(OutputArray image) = 0;
+	virtual void setDepth(InputArray  depth) = 0;
+	virtual void getDepth(OutputArray depth) = 0;
 
 private:
 
@@ -28,10 +31,14 @@ public:
 	OdometryFrameImplTMat(InputArray image = noArray());
 	~OdometryFrameImplTMat() {};
 
-	virtual void setImage(InputArray image) override;
+	virtual void setImage(InputArray  image) override;
+	virtual void getImage(OutputArray image) override;
+	virtual void setDepth(InputArray  depth) override;
+	virtual void getDepth(OutputArray depth) override;
 
 private:
 	TMat image;
+	TMat depth;
 };
 
 template<typename TMat>
@@ -43,7 +50,25 @@ OdometryFrameImplTMat<TMat>::OdometryFrameImplTMat(InputArray _image)
 template<typename TMat>
 void OdometryFrameImplTMat<TMat>::setImage(InputArray _image)
 {
-	image = getTMat<TMat>(_image);
+	this->image = getTMat<TMat>(_image);
+}
+
+template<typename TMat>
+void OdometryFrameImplTMat<TMat>::getImage(OutputArray _image)
+{
+	//_image = getTMat<TMat>(this->image);
+}
+
+template<typename TMat>
+void OdometryFrameImplTMat<TMat>::setDepth(InputArray _depth)
+{
+	this->depth = getTMat<TMat>(_depth);
+}
+
+template<typename TMat>
+void OdometryFrameImplTMat<TMat>::getDepth(OutputArray _depth)
+{
+	//_depth = getTMat<TMat>(this->depth);
 }
 
 
